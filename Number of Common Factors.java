b92a1998b9c61f9a9637847b9232db14b5d6ede0
Naman Talwar <89263331+namantalwar@users.noreply.github.com>
2023-07-03 22:56:09 +0530

    Create Number of Common Factors.java

---
+++ Number of Common Factors.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int x  = Math.min(a,b);
+        int c =0;
+        for(int i =1;i<=x;i++)
+        {
+            if(a%i==0&&b%i==0)
+            {
+                c++; 
+            }
+        }
+        return c;
+    }
+}
